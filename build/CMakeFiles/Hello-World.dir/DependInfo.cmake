c023ba7e0ba956b9cccd008084ab3e87f924e092
RAghavSaboo159 <raghavsaboo_rs@outlook.com>
2021-03-23 12:36:46 +0530

    src

---
+++ build/CMakeFiles/Hello-World.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ls/assgn/cg/ass1/Hello-World/source/game.cpp" "/home/ls/assgn/cg/ass1/Hello-World/build/CMakeFiles/Hello-World.dir/source/game.cpp.o"
+  "/home/ls/assgn/cg/ass1/Hello-World/source/main.cpp" "/home/ls/assgn/cg/ass1/Hello-World/build/CMakeFiles/Hello-World.dir/source/main.cpp.o"
+  "/home/ls/assgn/cg/ass1/Hello-World/source/resource_manager.cpp" "/home/ls/assgn/cg/ass1/Hello-World/build/CMakeFiles/Hello-World.dir/source/resource_manager.cpp.o"
+  "/home/ls/assgn/cg/ass1/Hello-World/source/shader.cpp" "/home/ls/assgn/cg/ass1/Hello-World/build/CMakeFiles/Hello-World.dir/source/shader.cpp.o"
+  "/home/ls/assgn/cg/ass1/Hello-World/source/texture.cpp" "/home/ls/assgn/cg/ass1/Hello-World/build/CMakeFiles/Hello-World.dir/source/texture.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_INCLUDE_NONE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../source"
+  "../libraries/glfw/include"
+  "../libraries/glad/include"
+  "../libraries/glm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ls/assgn/cg/ass1/Hello-World/build/libraries/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/ls/assgn/cg/ass1/Hello-World/build/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
